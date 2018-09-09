aaa3eb2c1768c351bf456471bed524e1e09987bd
SudhanshuKumar <sid_1590@yahoo.in>
2018-09-09 20:59:14 +0530

    added code for db connection, session maker and user helper function

--- application.py
+++ application.py
@@ -1,1 +1,48 @@
 # !/usr/bin/python3
+
+from flask import Flask, render_template, url_for, request
+from flask import redirect, flash, jsonify, make_response
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem, User
+
+app = Flask(__name__)
+
+# Connect to database
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+# Create session
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+		
+# end of file
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
